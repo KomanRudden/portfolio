1aa755c1060acf9a6dbfdb81034629c0bf63d64e
Koman Rudden <komanrudden@gmail.com>
2020-12-24 08:13:03 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './widgets/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full',
+  },{
+      path: 'home',
+      component: HomeComponent
+    },{
+      path: 'development',
+      loadChildren: () => import('./core/development/development.module').then(m => m.DevelopmentModule)
+  },{
+  path: '**',
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
+import { StaticService } from './service/static.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuItems } from './widgets/menu/menu-items';
+import { WidgetsModule } from './widgets/widgets.module';
+import { HomeComponent } from './widgets/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { HighlightPlusModule } from 'ngx-highlightjs/plus';
+import * as hljs from 'highlight.js';
+(document.defaultView as any).hljs = hljs;
+
+export function getHighlightLanguages() {
+  return {
+    xml: () => import('highlight.js/lib/languages/xml'), 
+    typescript: () => import('highlight.js/lib/languages/typescript'),
+    scss: () => import('highlight.js/lib/languages/scss'),
+    java: () => import('highlight.js/lib/languages/java'),
+    plaintext: () => import('highlight.js/lib/languages/plaintext'),
+  };
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    WidgetsModule,
+    HighlightModule,
+    HighlightPlusModule
   ],
-  providers: [],
+  exports: [
+    HighlightModule,
+    HighlightPlusModule
+  ],
+  providers: [
+    MenuItems,
+    StaticService,
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        languages: getHighlightLanguages()
+     }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
