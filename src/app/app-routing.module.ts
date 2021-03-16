fabc851b9611b751462e2abc0576bcee4d73e782
Koman Rudden <komanrudden@gmail.com>
2021-03-16 20:50:17 +0200

    Started adding various technology topics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,27 @@ const routes: Routes = [{
   path: '',
   redirectTo: 'home',
   pathMatch: 'full',
-  },{
+    },{
       path: 'home',
       component: HomeComponent
     },{
+      path: 'cloud',
+      loadChildren: () => import('./core/cloud/cloud.module').then(m => m.CloudModule)
+    },{      
       path: 'development',
       loadChildren: () => import('./core/development/development.module').then(m => m.DevelopmentModule)
-  },{
-  path: '**',
-  component: HomeComponent
+    },{
+      path: 'frameworks',
+      loadChildren: () => import('./core/frameworks/frameworks.module').then(m => m.FrameworksModule)
+    },{
+      path: 'general',
+      loadChildren: () => import('./core/general/general.module').then(m => m.GeneralModule)
+    },{
+      path: 'databases',
+      loadChildren: () => import('./core/databases/databases.module').then(m => m.DatabasesModule)
+    },{      
+      path: '**',
+      component: HomeComponent
 }];
 
 @NgModule({
