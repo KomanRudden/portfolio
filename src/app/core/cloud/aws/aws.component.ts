73790ae7a93cbfae39a3885b0f045ca4d2c3788e
Koman Rudden <komanrudden@gmail.com>
2021-03-16 21:17:58 +0200

    Changed aws cli gist endpoint

--- src/app/core/cloud/aws/aws.component.ts
+++ src/app/core/cloud/aws/aws.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AwsComponent {
 
-  codeUrl = 'https://gist.githubusercontent.com/KomanRudden/2a541a0faeb0c91d5f0fd4f3711f48ee/raw/f5b8cf047ed15050f9f8e9445f24bc2d01fe6228/gistfile1.txt';
+  codeUrl = 'https://gist.githubusercontent.com/KomanRudden/2a541a0faeb0c91d5f0fd4f3711f48ee/raw/7e7277fde93fc94e460c1272e6dc2598f6a6a8f8/aws';
 
 }
